5adbf359ca759d3d4f3523dbce04c0b7d7c5f369
djole-petrovic-js <djordjepetrovic@Djordjes-MacBook-Pro.local>
2024-02-06 08:56:16 +0100

    Fixed next.js config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // debuggg - ovo bi trebalo sad da se skloni jel? ne treba mi vise.
-  images: {
-    domains: ['otkup.gvs-srbija.rs']
-  },
-}
+const nextConfig = {}
 
-// module.exports = nextConfig
-
-// debuggg - ovo mozda ne radi, proveriti
-export default nextConfig
+module.exports = nextConfig
